5d8a4401164982ed5085467d4f999112aa3d96c9
Tomasz Rybarczyk <paluho@gmail.com>
2018-02-27 16:03:50 +0100

    Drop parent configuration from Options

--- src/Pux/Clappr.purs
+++ src/Pux/Clappr.purs
@@ -1,11 +1,23 @@
 module Pux.Clappr where
 
-import Clappr (NativeOptions)
+import Clappr (OptionsBase, NativeOptions)
+import Data.Maybe (Maybe(..))
+import Data.Nullable (toNullable)
 import Pux.DOM.HTML (HTML)
 import Pux.Renderer.React (reactClassWithProps)
 import React (ReactClass)
 
 foreign import clapprImpl ∷ ∀ r. ReactClass (NativeOptions r)
+
+type Options = OptionsBase ()
+
+toNativeOptions ∷ Options → NativeOptions ()
+toNativeOptions options =
+  { source: options.source
+  , plugins: []
+  , parentId: toNullable Nothing
+  , parent: toNullable Nothing
+  }
 
 clappr ∷ ∀ env r. NativeOptions r → (HTML env → HTML env)
 clappr opts =
